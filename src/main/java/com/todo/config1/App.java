3e38073af3c5b862016646c44324586510b71ed0
beethom <neyoo716@gmail.com>
2021-10-18 01:24:54 -0400

    The start of the project.

---
+++ src/main/java/com/todo/config1/App.java
@@ -0,0 +1,13 @@
+package com.todo.config1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
